94828b736061ba7a9a295cee891b776d4105f308
anuoluwapo <oluwapoanu87@gmail.com>
2023-09-02 07:26:34 +0100

    Chore: updated code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from "react";
 
-
-const SearchForm = () => {
-  
+const SearchForm = ({ setInputValue, inputValue }) => {
   return (
-
     <div className="mb-10 p-4">
-     <div className="max-w-md mx-auto flex">
-      <input
-        type="text"
-        placeholder="Search..."
-        // value={capsules}
-        // onChange={(e) => setCapsules(e.target.value)}
-        className="w-full border p-2 rounded-l focus:outline-none"
-      />
-       <button
-        // onClick={e => handleSearch(e.target.value)}
-        className="bg-red-500 text-white px-8 py-2 rounded-r-md rounded-l-md ml-10 focus:outline-none"
-      >
-        Search
-      </button>
+      <div className="max-w-md mx-auto flex">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="w-full border p-2 rounded-l focus:outline-none"
+        />
+        <button
+          // onClick={e => handleSearch(e.target.value)}
+          className="bg-red-500 text-white px-8 py-2 rounded-r-md rounded-l-md ml-10 focus:outline-none"
+        >
+          Search
+        </button>
+      </div>
     </div>
-    </div> 
   );
 };
 
